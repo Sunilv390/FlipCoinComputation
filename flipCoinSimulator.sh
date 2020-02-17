d170b240881b837705be003d497300a1da71211f
Sunilv390 <sunilv390@gmail.com>
2020-02-17 12:57:56 +0530

    Added: Flipcoin to get heads or tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,10 @@
-Welcome to FlipCoin Simulator
+#!/bin/bash -x
+
+#RANDOM VALUE FOR HEADS AND TAIL
+randomFlip=$((RANDOM%2))
+if [ $randomFlip -eq 1 ]
+then
+	echo "Its a Head"
+else
+	echo "Its a Tail"
+fi
