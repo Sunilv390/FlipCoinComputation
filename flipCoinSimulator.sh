ecfe2aae003670ed089fa2d1ec295a6c3b6508c6
Sunilv390 <sunilv390@gmail.com>
2020-02-17 18:55:16 +0530

    Added: stores single combination in dictionary and prints percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,27 @@
 #!/bin/bash -x
 
+#CONSTANT
+ISHEAD=1
+
+#USERINPUT
+declare -A coin
+read -p "Enter number of times to flip " number
+
 #RANDOM VALUE FOR HEADS AND TAIL
-randomFlip=$((RANDOM%2))
-if [ $randomFlip -eq 1 ]
-then
-	echo "Its a Head"
-else
-	echo "Its a Tail"
-fi
+for (( counter=0; counter<number; counter++ ))
+do
+	randomFlip=$((RANDOM%2))
+	if [ $randomFlip -eq $ISHEAD ]
+	then
+		coin[$counter]=H
+		((headCount++))
+		Headpercent=`echo "scale=2; $headCount*100/$number" | bc`
+	else
+		coin[$counter]=T
+		((tailCount++))
+		Tailpercent=`echo "scale=2; $tailCount*100/$number" | bc`
+	fi
+done
+echo "Singlet Combination is "${coin[@]}
+echo "Head percentage is $Headpercent%"
+echo "Tail percentage is $Tailpercent%"
