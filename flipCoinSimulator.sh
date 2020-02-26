2d9de651f3e85350055f76542b56457f1b6ab271
Sunilv390 <sunilv390@gmail.com>
2020-02-26 11:58:17 +0530

    Refactor: Indentations are changed

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,79 +5,68 @@ ISHEAD=1
 SINGLE=1
 DOUBLE=2
 TRIPLE=3
-
-#DECLARE A DICTIONARY TO STORE COMBINATIONS
+#STORING COMBINATIONS
 declare -A singleFlip
 declare -A doubleFlip
 declare -A tripleFlip
-
 read -p "Enter how many times to flip a coin " flipCoin
-
-#FUNCTION TO CHECK DOUBLET COMBINATION
+#CHECK DOUBLET COMBINATION
 function isFlip(){
-local NumberCoin=$1
-for (( flip=0; flip<$flipCoin; flip++ ))
-do
-   for (( coin=0; coin<$NumberCoin; coin++ ))
-   do
-      randomCheck=$((RANDOM%2))
-      if [ $randomCheck -eq $ISHEAD ]
-      then
-         echo "Head"
-         coinSide+=H
-      else
-         echo "Tails"
-         coinSide+=T
-      fi
-   done
-#STORING THE COMBINATION IN DICTIONARY
-   if [ $NumberCoin -eq $SINGLE ]
-   then
-      ((singleFlip[$coinSide]++))
-      echo ${!singleFlip[@]}
-   elif [ $NumberCoin -eq $DOUBLE ]
-   then
-      ((doubleFlip[$coinSide]++))
-      echo ${!doubleFlip[@]}
-   elif [ $NumberCoin -eq $TRIPLE ]
-	then
-		((tripleFlip[$coinSide]++))
-      echo ${!tripleFlip[@]}
-   fi
-   coinSide=""
-done
+	local NumberCoin=$1
+	for (( flip=0; flip<$flipCoin; flip++ ))
+	do
+   	for (( coin=0; coin<$NumberCoin; coin++ ))
+   	do
+      	randomCheck=$((RANDOM%2))
+      	if [ $randomCheck -eq $ISHEAD ]
+      	then
+         	coinSide+=H
+      	else
+         	coinSide+=T
+      	fi
+   	done
+   	if [ $NumberCoin -eq $SINGLE ]
+   	then
+      	((singleFlip[$coinSide]++))
+   	elif [ $NumberCoin -eq $DOUBLE ]
+   	then
+      	((doubleFlip[$coinSide]++))
+   	elif [ $NumberCoin -eq $TRIPLE ]
+		then
+			((tripleFlip[$coinSide]++))
+   	fi
+   	coinSide=""
+	done
 }
-echo "Enter 1 for Single Combination"
-echo "Enter 2 for Double Combination"
-echo "Enter 3 for Triple Combination"
-
+printf "Enter 1 for Single Combination\n"
+printf "Enter 2 for Double Combination\n"
+printf "Enter 3 for Triple Combination\n"
 #FUNCTION TO GET PERCENTAGE
 function getPercent(){
-local NumberCoin=$1
-if [ $NumberCoin -eq $SINGLE ]
-then
-	for index in ${!singleFlip[@]}
-   do
-		singleFlip[$index]=`echo "scale=2; ${singleFlip[$index]}*100/$flipCoin" | bc`
-   done
-elif [ $NumberCoin -eq $DOUBLE ]
-then
-   for index in ${!doubleFlip[@]}
-   do
-		doubleFlip[$index]=`echo "scale=2; ${doubleFlip[$index]}*100/$flipCoin" | bc`
-   done
-elif [ $NumberCoin -eq $TRIPLE ]
-then
-	for index in ${!tripleFlip[@]}
-	do
-		tripleFlip[$index]=`echo "scale=2; ${tripleFlip[$index]}*100/$flipCoin" | bc`
-	done
-else
-	echo "Enter choice from 1-3"
-fi
+	local NumberCoin=$1
+	if [ $NumberCoin -eq $SINGLE ]
+	then
+		for index in ${!singleFlip[@]}
+   	do
+			singleFlip[$index]=`echo "scale=2; ${singleFlip[$index]}*100/$flipCoin" | bc`
+   	done
+	elif [ $NumberCoin -eq $DOUBLE ]
+	then
+   	for index in ${!doubleFlip[@]}
+   	do
+			doubleFlip[$index]=`echo "scale=2; ${doubleFlip[$index]}*100/$flipCoin" | bc`
+   	done
+	elif [ $NumberCoin -eq $TRIPLE ]
+	then
+		for index in ${!tripleFlip[@]}
+		do
+			tripleFlip[$index]=`echo "scale=2; ${tripleFlip[$index]}*100/$flipCoin" | bc`
+		done
+	else
+		printf "Enter choice from 1-3\n"
+	fi
 }
-
-#SWITCH CASE FOR CHOICES
+#PRINTING CHOICES
 read -p "Enter your choice " choice
 case $choice in
    $SINGLE)
@@ -92,5 +81,5 @@ case $choice in
 	isFlip $TRIPLE
 	getPercent $TRIPLE
 	;;
-*)
+	*)
 esac
